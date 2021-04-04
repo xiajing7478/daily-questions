3ffe7dcd7b37f6fb5c6996921e8a08cbea5ebd4c
xiajing7478 <260594736@qq.com>
2021-04-04 21:59:09 +0800

    vue原理'

--- js/vue响应式原理/MVVM.js
+++ js/vue响应式原理/MVVM.js
@@ -1,7 +1,7 @@
 class MVVM {
-  constructor({ data, el }) {
-    this.data = data
-    this.el = el
+  constructor(options) {
+    this.$data = options.data
+    this.$el = options.el
     this.init()
     this.initDom()
   }
@@ -9,4 +9,35 @@ class MVVM {
 
   }
   initDom()
+}
+
+/**
+ * 数据劫持
+ */
+class Observe {
+  constructor(data) {
+    this.observe(data)
+  }
+  observe(data) {
+    if (data && typeof data === 'object') {
+      for(let key in data) {
+        this.defineReactive(data, key, data[key])
+      }
+    }
+  }
+  defineReactive(obj, key, value) {
+    this.observe(data)
+    Object.defineProperty(obj, {
+      get() {
+        return value
+      },
+      set(newVal) {
+        if (value != newVal) {
+          // 如果赋值还是一个对象的话，需要再次转化get,set方法
+          this.observe(newVal)
+          value = newVal
+        }
+      }
+    })
+  }
 }
